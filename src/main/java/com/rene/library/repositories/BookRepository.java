1ca94ebb8a023a4a5d64866fe96d6444b52cfdb0
Renê <renejr.arraes286@gmail.com>
2021-06-15 18:18:03 -0300

    create repositories

---
+++ src/main/java/com/rene/library/repositories/BookRepository.java
@@ -0,0 +1,12 @@
+package com.rene.library.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rene.library.models.Book;
+
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+	
+}
